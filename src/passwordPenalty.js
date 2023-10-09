9e2196f4a11ac2e1f02f59643be16b6194993d28
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-09 17:54:19 +0200

    feat: implement penaltyPoints function[passwordPenalty.js]

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,10 +7,20 @@
  */
 export default function penaltyPoints(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
+  if (!password) return 0;
   if (typeof password !== "string") password = String(password);
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+  return password.split("").reduce((occurences, letter, index) => {
+    if (password.slice(0, index).includes(letter)) return occurences;
+    const regex = new RegExp(`${letter}${letter}+`, "g");
+    const results = password.match(regex);
+    if (results) {
+      results.map((result) =>
+        result.length % 2 === 0
+          ? (occurences += result.length / 2)
+          : (occurences += result.length - 1)
+      );
+    }
+    return occurences;
+  }, 0);
 }
